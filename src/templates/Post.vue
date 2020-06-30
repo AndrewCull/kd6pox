49de945ad307b1721d8dfb2f8506429f9f312d03
Andrew Cull <andrew@agema.io>
2020-06-30 14:37:07 -0600

    change image path

--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -1,22 +1,23 @@
 <template>
   <Layout>
     <article>
-    <h1 class="mb0 pb0 lh-title f3 f1-ns lh-title">{{ $page.post.title }}</h1>
-    <span class="mt0 black-40"><small>Last Updated {{$page.post.date | moment("DD MMMM YYYY") }}</small></span>
+      <h1 class="mb0 pb0 f3 f1-ns" style="line-height: .75;">{{ $page.post.title }}</h1>
+      <small
+        class="black-40"
+        
+      >Last Updated {{$page.post.date | moment("DD MMMM YYYY") }}</small>
+      <div v-if="$page.post.tags" class="mb5">
+        <div v-for="tag in $page.post.tags" :key="tag.id" class="mt2">
+          <g-link :to="tag.path" class="link ttc b f7">{{tag.id}}</g-link>
+        </div>
+      </div>
 
-  <div v-if="$page.post.tags">
-  <div v-for="tag in $page.post.tags" :key="tag.id">
-    <g-link :to="tag.path">{{tag.id}}</g-link>
-  </div>
-  </div>
-  
-    
-    <div v-html="$page.post.content"/>
-    <div class="center tc mt4 ba b--black-10 pv3 br2">
-      <span class="black-30">Share on Social:</span>
-    <share-it icons :url="$page.post.path"/>
-  </div>
-  </article>
+      <div v-html="$page.post.content" />
+      <div class="center tc mt4 ba b--black-10 pv3 br2">
+        <span class="black-30">Share on Social:</span>
+        <share-it icons :url="$page.post.path" />
+      </div>
+    </article>
   </Layout>
 </template>
 
@@ -43,20 +44,20 @@ export default {
       title: this.$page.post.title,
       meta: [
         {
-          key: 'og:description',
-          name: 'og:description',
-          content: this.$page.post.excerpt,
+          key: "og:description",
+          name: "og:description",
+          content: this.$page.post.excerpt
         },
 
         {
-          key: 'twitter:description',
-          name: 'twitter:description',
-          content: this.$page.post.excerpt,
-        },
-      ],
-    }
+          key: "twitter:description",
+          name: "twitter:description",
+          content: this.$page.post.excerpt
+        }
+      ]
+    };
   },
-   computed: {
+  computed: {
     posts() {
       return this.$page.tag.belongsTo.edges.map(e => e.node);
     }
